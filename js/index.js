06592e6c87a06fc2351ec9eb3ad0736f852b4624
ebraheemabbas <ebraheemabbas51@gmail.com>
2019-09-18 16:32:30 +0300

    clone node and add trail video Realated #2

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ var character_objects_array;
 var url_character_objects = "https://jeffreylancaster.github.io/game-of-thrones/data/characters.json";
 var filterd_character_objects_array = [];
 
-var procces_objects = (charc_array) => {
+var procces_objects = (charc_array,callback) => {
     charc_array.forEach(e => {
         var obj = {};
         Object.keys(e).forEach(element => {
@@ -15,23 +15,53 @@ var procces_objects = (charc_array) => {
         });
         filterd_character_objects_array.push(obj);
     });
+    console.log(filterd_character_objects_array[0].characterName);
+    callback();
 
 }
 
-var fetching = () => {
+var fetching = (callback) => {
     fetch(url_character_objects)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            character_objects_array = data.characters.filter(e => (e.royal == true));
-            procces_objects(character_objects_array);
+            character_objects_array = data.characters.filter(e => (e.royal == true && e.characterImageFull!=null));
+            procces_objects(character_objects_array,callback);
         })
         .catch(function (error) {
             console.log(error);
         })
 }
+function cleanCard(Card){
+    Card.getElementsByClassName("chracter_killer")[0].textContent="killed by : ";
+    Card.getElementsByClassName("chracter_origin")[0].textContent="origin :";
+    Card.getElementsByClassName("chracter_real_name")[0].textContent="actor name :"
+    Card.getElementsByClassName("chracter_nick_name")[0].textContent="NICK NAME :";
+}
+function modifyByDom(){
+    var card=document.getElementsByClassName("flip-card")[0];
+    let i=0;
+    filterd_character_objects_array.forEach(e=>{
+        card.getElementsByClassName("chracter_killer")[0].textContent+=e.killedBy==undefined ?"":"NONE";
+        card.getElementsByClassName("chracter_name")[0].textContent=e.characterName;
+        card.getElementsByClassName("chracter_nick_name")[0].textContent+=e.nickname==undefined ?"":"NONE";
+        card.getElementsByClassName("chracter_origin")[0].textContent+=e.houseName;
+        card.getElementsByClassName("chracter_real_name")[0].textContent+=e.actorName==undefined?"":"NONE";
+        card.getElementsByClassName("chracter_image")[0].src=e.characterImageFull;
+        if(i<filterd_character_objects_array.length-1){
+      var cln = card.cloneNode(true);
+      cleanCard(cln)
+      card.parentElement.appendChild(cln);
+      card=cln;
+        }
+        i=i+1;
+    });
 
-fetching();
 
-console.log(filterd_character_objects_array);
+}
+fetching(modifyByDom);
+
+
+
+
